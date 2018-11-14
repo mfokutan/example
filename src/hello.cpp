4a141a34a2fc13b9c3aa3d6bad77f6aacacbe2bf
Muhammet Okutan <muhammet.okutan@ericsson.com>
2018-11-14 15:01:35 +0300

    Initial commit

---
+++ src/hello.cpp
@@ -0,0 +1,46 @@
+//============================================================================
+// Name        : hello.cpp
+// Author      : Fatih
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+	 void setA(int a) {
+		this->a = a;
+	}
+
+	 int getA() {
+		return this->a;
+	}
+
+	 void displayA( ) {
+		cout << this->a << endl;
+	}
+
+private:
+	 int a;
+};
+
+int main()
+{
+	Base base = Base();
+	base.setA(20);
+	base.displayA(); // 20
+//	Base *base2 = &base;
+//	base2->setA(30);
+//	base2->displayA();
+//	base.displayA();
+
+	Base &base2 = base;
+	base2.setA(30);
+	base2.displayA(); // 30
+	base.displayA(); // 20
+
+	return 0;
+}
